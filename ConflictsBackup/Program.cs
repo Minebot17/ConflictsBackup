97488939b9217e476af72692c53449709b91584b
Minebot <serpinin@mail.ru>
2021-03-18 15:02:29 +0300

    init

---
+++ ConflictsBackup/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConflictsBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory(); // директория из консоли
+            Process cmd = new ()
+            {
+                StartInfo =
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                }
+            };
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine("cd ");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            
+            cmd.WaitForExit();
+            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+    }
+}
