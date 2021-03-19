f39c97982c3f8e7d9ae36dd5577c8fe0d4ee3a5e
Minebot <serpinin@mail.ru>
2021-03-19 10:11:56 +0300

    ver 1.0

--- ConflictsBackup/Program.cs
+++ ConflictsBackup/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace ConflictsBackup
 {
     class Program
     {
+        private static string[] ourFoldes = { "Assets", "Packages", "ProjectSettings" };
+        
         static void Main(string[] args)
         {
-            string path = Directory.GetCurrentDirectory(); // директория из консоли
+            string currentDirectory = Directory.GetCurrentDirectory();
             Process cmd = new ()
             {
                 StartInfo =
@@ -22,12 +27,28 @@ namespace ConflictsBackup
             };
             cmd.Start();
 
-            cmd.StandardInput.WriteLine("cd ");
+            cmd.StandardInput.WriteLine("cd " + currentDirectory);
+            cmd.StandardInput.WriteLine("git diff --name-only --diff-filter=U");
             cmd.StandardInput.Flush();
             cmd.StandardInput.Close();
             
             cmd.WaitForExit();
-            Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+            string[] conflictsPaths = cmd.StandardOutput.ReadToEnd()
+                .Replace("\r", "")
+                .Split("\n")
+                .Where(str => ourFoldes.Any(str.StartsWith) && File.Exists(str))
+                .ToArray();
+            
+            foreach (string path in conflictsPaths)
+                File.Copy(path, path + ".backup", true);
+            
+            List<string> tmpList = conflictsPaths.ToList();
+            tmpList.Insert(0, "python govno");
+            conflictsPaths = tmpList.ToArray();
+            
+            File.WriteAllLines(currentDirectory + "\\.git\\backupsConflicts", conflictsPaths.Select(str => str + ".backup"), Encoding.UTF8);
+            
+            Console.WriteLine("Backup success");
         }
     }
 }
